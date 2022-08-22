5ed11ea18ab0a62871fa285961897d4250d943c5
Juliana Sinnott <julianasinnott@outlook.com>
2022-08-21 22:13:38 -0300

    feat: implements data in db

--- db.js
+++ db.js
@@ -1,36 +1,8 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const recipes = require("./data/recipes");
+const partners = require("./data/partners");
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
+  recipes,
+  partners
 }
 
-
-// const items = require("./data/items");
-
-// module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
- 
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
-
-//   return {
-//     items,
-//     tags,
-//     brands,
-//     itemTypes,
-//   };
-// };
